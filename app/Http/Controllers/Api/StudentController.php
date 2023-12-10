093d568c0d7ae7c0f16a175c38e0a6c19a84a493
Evans Roy <evansroysir@gmail.com>
2023-12-10 10:48:22 +0300

    Add edit functionality

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -104,7 +104,19 @@ class StudentController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $student = Student::find($id);
+
+        if($student){
+            return response()->json([
+                'status' => 200,
+                'student' => $student,
+            ], 200);
+        }else{
+            return response()->json([
+                'status' => 404,
+                'message' => 'No  Such Student Found'
+            ],404);
+        }
     }
 
     /**
